e1e0b7bca16892d36beb2a1416a86d63b6c8ef42
Aditya Prakash <prakashadityaap@gmail.com>
2025-03-22 16:18:40 -0400

    .

--- client/app/routes/register.tsx
+++ client/app/routes/register.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Form, useActionData , useNavigate } from "@remix-run/react";
+import { Form, useActionData, useNavigate } from "@remix-run/react";
 
 
 import { registerUser } from "~/utils/api";
